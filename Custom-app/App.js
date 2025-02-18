70d9fc6ad755129b01a36e66e3aaf46021e246e8
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-02-18 09:26:53 +0600

    Write all the steps for create custom app using react

--- Custom-app/App.js
+++ Custom-app/App.js
@@ -1,3 +1,7 @@
 // How can we create custom app using Node.js and React
 
 //Step1: First Need to create shopify Partner account.
+//Step2: Use Shopify's CLI or create a new Node.js project with React.
+//Step3: Authenticate app with Shopify using OAuth.
+//Step4: Develop app using React components to create UI elements.
+//Step5: Test your locally and then deploy it to a hosting platform like Heroku or Vercel.
